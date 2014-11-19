1bb6f35723fccd3539555192170a5f92700a1e0e
Peter Mösenthin <peter.moesenthin@gmail.com>
2014-11-19 18:32:43 +0100

    split main initilization and add debug setup

--- src/main/java/RaspberryLife.java
+++ src/main/java/RaspberryLife.java
@@ -2,19 +2,13 @@
 import data.DataBaseHelper;
 import data.Log;
 import data.SerialConnector;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.impl.StdSchedulerFactory;
 import server.RBLSocketServer;
 import server.RBLWebSocketServer;
 import util.Config;
 import util.NetworkUtil;
-
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.security.Timestamp;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Enumeration;
-import java.util.List;
 
 /**
  * Created by Peter Mösenthin.
@@ -27,13 +21,42 @@ public class RaspberryLife {
     public static Thread serverThread = null;
     public static Thread webServerThread = null;
 
+    public static boolean runDebugSetup = false;
+
+    //----------------------------------------------------------------------------------------------
+    //                                      MAIN
+    //----------------------------------------------------------------------------------------------
 
     public static void main(String[] args){
         Log.printLogHeader();
         Config.readConfig();
         //Config.dumpConfig();
         NetworkUtil.listIPAddresses();
-        // RaspberryHome WebSocketServer
+        startSocketServer();
+        startWebSocketServer();
+        initSerialConnection();
+        initDatabase();
+        if(runDebugSetup){
+            initScheduler();
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //                                      STARTUP
+    //----------------------------------------------------------------------------------------------
+
+    public static void startSocketServer(){
+        serverThread = new Thread(new Runnable() {
+            public void run() {
+                Log.add(DEBUG_TAG, "Starting Java socket server");
+                RBLSocketServer server = new RBLSocketServer();
+                server.start(Config.get().getInt("socket.java_port"));
+            }
+        });
+        serverThread.start();
+    }
+
+    public static void startWebSocketServer(){
         webServerThread = new Thread(new Runnable() {
             public void run() {
                 Log.add(DEBUG_TAG,
@@ -42,25 +65,34 @@ public class RaspberryLife {
                 webServer.start();
             }
         });
-
-        // RaspberryHome Application Server
-        serverThread = new Thread(new Runnable() {
-                public void run() {
-                Log.add(DEBUG_TAG, "Starting Java socket server");
-                RBLSocketServer server = new RBLSocketServer();
-                server.start(Config.get().getInt("socket.java_port"));
-            }
-        });
-
-        // Start threads
         webServerThread.start();
-        serverThread.start();
+    }
 
-        // Initialize the serial connector for module communication
+    // Initialize the serial connector for module communication
+    public static void initSerialConnection(){
         SerialConnector.init();
+    }
 
-        // Initialize database
+    public static void initDatabase(){
+        Log.add(DEBUG_TAG, "Initializing database");
         DataBaseHelper.setUpInitial();
+    }
+
+    public static void initScheduler(){
+        Log.add(DEBUG_TAG, "Initializing scheduler");
+        Scheduler scheduler = null;
+        try {
+            scheduler = StdSchedulerFactory.getDefaultScheduler();
+        } catch (SchedulerException e) {
+            Log.add(DEBUG_TAG, "Unable to create scheduler");
+        }
+        if(scheduler != null){
+            try {
+                scheduler.start();
+            } catch (SchedulerException e) {
+                Log.add(DEBUG_TAG, "Unable to start scheduler");
+            }
+        }
 
     }
 
