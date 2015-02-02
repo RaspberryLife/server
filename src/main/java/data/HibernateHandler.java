f4b83232f0c25acf0eee85818b35ab4b5ffb0e03
Peter Mösenthin <peter.moesenthin@gmail.com>
2015-02-02 14:17:44 +0100

    clean up database handling and move access to databaseservice

--- src/main/java/data/HibernateHandler.java
+++ src/main/java/data/HibernateHandler.java
@@ -6,9 +6,12 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
+import protobuf.RblProto;
 import util.Log;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 /**
  * Created by Peter Mösenthin.
@@ -18,29 +21,45 @@ public class HibernateHandler {
     public final String DEBUG_TAG = HibernateHandler.class.getSimpleName();
 
     private SessionFactory sessionFactory;
-    private MySqlConnection mySqlConnection;
 
+    public enum CreationMode {
+        CREATE, // creates the schema, destroying previous data.
+        CREATE_DROP, //drop the schema at the end of the session.
+        UPDATE, //update the schema.
+        VALIDATE //validate the schema, makes no changes to the database.
+    }
+
+    public enum DataType{
+        LOGIC,
+        USER,
+        MESSAGE
+    }
 
     /**
      * Initialize a new server session. Create database if needed.
      */
-    public void initSession(){
-        Log.add(DEBUG_TAG, "Initializing session");
-        if(mySqlConnection == null){
-            mySqlConnection = new MySqlConnection();
-        }
-        String creationMode = null;
+    public void initSession(CreationMode mode){
 
-        if(mySqlConnection.open()){
-            if(!mySqlConnection.databaseExists()){
-                Log.add(DEBUG_TAG, "Database does not exist creating new.   ");
-                createInitialStructure();
-            }else {
+        String creationMode = null;
+        switch (mode){
+            case CREATE:
+                creationMode = "create";
+                break;
+            case CREATE_DROP:
+                creationMode = "create-drop";
+                break;
+            case UPDATE:
                 creationMode = "update";
-            }
+                break;
+            case VALIDATE:
+                creationMode = "validate";
+                break;
         }
+        Log.add(DEBUG_TAG, "Initializing session (MODE: " + creationMode + ")");
         sessionFactory = buildSessionFactory(creationMode);
     }
+
+
 
     /**
      * Build Hibernate session factory
@@ -70,140 +89,74 @@ public class HibernateHandler {
         return sessionFactory;
     }
 
-    /**
-     * Create basic database structure.
-     */
-    private void createInitialStructure(){
-        if(mySqlConnection.open()){
-            mySqlConnection.createDatabase();
-            mySqlConnection.close();
-        }
-    }
-
-
-
-
     //----------------------------------------------------------------------------------------------
-    //                                      TESTING
+    //                                      INSERT
     //----------------------------------------------------------------------------------------------
 
-    public void readLogic(){
-        Log.add(DEBUG_TAG, "Running read test");
-        Session session = sessionFactory.openSession();
-        List logic_list = session.createQuery("from Logic").list();
+    public void insertList(List<Object> list){
 
-        for(Object o : logic_list){
-            Logic l = (Logic) o;
-            //printLogic(l);
-        }
-        session.close();
     }
 
-    public void writeTestLogic(){
+    public void insert(Object object){
         try {
-            Log.add(DEBUG_TAG, "Running write test");
             Session session = sessionFactory.openSession();
             session.beginTransaction();
 
-            Actuator a = new Actuator();
-            a.setType(Actuator.TYPE_CLIENT);
-            a.setName("peters_nexus5");
-
-            Actuator b = new Actuator();
-            b.setType(Actuator.TYPE_MODULE);
-            b.setName("Heizung-Wohnzimmer");
-
-            Actuator c = new Actuator();
-            c.setType(Actuator.TYPE_SYSTEM);
-            c.setName("server_1.1.2");
-
-            Logic l1 = new Logic();
-            l1.setName("test_logic_666");
-            l1.setExecution_requirement(Logic.EXECUTION_REQUIREMENT_ALL);
-
-            ExecutionFrequency ef = new ExecutionFrequency();
-            ef.setType(ExecutionFrequency.TYPE_DAILY);
-            ef.setHour(17);
-            ef.setMinute(40);
-
-            l1.setExecution_frequency(ef);
-
-            LogicInitiator li = new LogicInitiator();
-            li.setActuator(a);
-            Condition co = new Condition();
-            co.setField_id(1);
-            co.setThreshold_over(24);
-            li.setCondition(co);
-
-            LogicInitiator li1 = new LogicInitiator();
-            li1.setActuator(b);
-            Condition co1 = new Condition();
-            co1.setField_id(2);
-            co1.setThreshold_under(10);
-            li1.setCondition(co1);
-
-            LogicReceiver lr = new LogicReceiver();
-            lr.setActuator(c);
-            Instruction i = new Instruction();
-            i.setField_id(12);
-            i.setParameters("hallo du wurst");
-            lr.setInstruction(i);
-
-            l1.getLogic_initiator().add(li);
-            l1.getLogic_initiator().add(li1);
-            l1.getLogic_receiver().add(lr);
-
-            //printLogic(l1);
-
-            session.save(l1);
+            session.save(object);
 
             session.getTransaction().commit();
             session.close();
         }catch (Exception e){
-            e.printStackTrace();
+            Log.add(DEBUG_TAG, "Could not write data");
         }
     }
 
+    //----------------------------------------------------------------------------------------------
+    //                                      READ
+    //----------------------------------------------------------------------------------------------
 
-    public void printLogic(Logic l){
-        try {
-            String init = "[";
-            String rec = "[";
 
-            if (l.getLogic_initiator() != null && l.getLogic_initiator().size() > 0) {
-                for (LogicInitiator lii : l.getLogic_initiator()) {
-                    init += "(";
-                    init += " Actuator: " + lii.getActuator().getName();
-                    init += " Condition (fid): " + lii.getCondition().getField_id();
-                    init += ")";
-                }
-            }
-
-            init += "]";
-            if (l.getLogic_receiver() != null && l.getLogic_receiver().size() > 0) {
-                for (LogicReceiver lrr : l.getLogic_receiver()) {
-                    rec += "(";
-                    rec += " Actuator: " + lrr.getActuator().getName();
-                    rec += " Instruction (fid)" + lrr.getInstruction().getField_id();
-                    rec += ")";
-                }
-            }
-            rec += "]";
-
-            String freq = "[";
-            freq += " Type: " + l.getExecution_frequency().getType();
-            freq += " Hour: " + l.getExecution_frequency().getHour();
-            freq += " Minute: " + l.getExecution_frequency().getMinute();
-            freq += "]";
-
-            Log.add(DEBUG_TAG,
-                    "Found logic: " + l.getName()
-                            + " Id: " + l.getLogic_id()
-                            + " Frequency: " + freq
-                            + " Initiator: " + init
-                            + " Receiver: " + rec);
-        }catch (Exception e){
-            e.printStackTrace();
+    public List readList(DataType type){
+        String query = null;
+        switch (type){
+            case LOGIC:
+                query = "from Logic";
+                break;
+            case USER:
+                break;
+            case MESSAGE:
+                break;
         }
+        Session session = sessionFactory.openSession();
+        List list = session.createQuery(query).list();
+        session.close();
+        return list;
     }
+
+    public Object read(DataType type){
+        switch (type){
+            case LOGIC:
+                break;
+            case USER:
+                break;
+            case MESSAGE:
+                break;
+        }
+
+        return null;
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //                                      UPDATE
+    //----------------------------------------------------------------------------------------------
+
+
+
+    //----------------------------------------------------------------------------------------------
+    //                                      DELETE
+    //----------------------------------------------------------------------------------------------
+
+
+
+
 }
--- src/main/java/system/service/DataBaseService.java
+++ src/main/java/system/service/DataBaseService.java
@@ -1,21 +1,12 @@
 package system.service;
 
-import com.google.common.eventbus.Subscribe;
 import data.HibernateHandler;
 import data.MySqlConnection;
-import data.model.Actuator;
-import data.model.Logic;
-import event.DataBaseEvent;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
+import data.model.*;
 import util.Log;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+
 
 /**
  * Created by Peter Mösenthin.
@@ -28,33 +19,197 @@ public class DataBaseService {
     private static final DataBaseService instance = new DataBaseService();
 
     private HibernateHandler hibernateHandler = new HibernateHandler();
+    private MySqlConnection mySqlConnection;
 
-    public static void register() {
-        EventBusService.register(instance);
+    public static DataBaseService getInstance(){
+        return instance;
     }
 
     private DataBaseService(){
     }
 
-    @Subscribe
-    public void handleDataBaseEvent(DataBaseEvent e){
-        switch(e.getMessage()){
-            case DataBaseEvent.START_SESSION:
-                hibernateHandler.initSession();
-                break;
-            case DataBaseEvent.RUN_TEST:
-                runHibernateWriteTest();
-                runHibernateReadTest();
-                break;
+    public void start(){
+        Log.add(DEBUG_TAG, "Starting...");
+        if(dataBaseAvailable()){
+            if(dataBaseSetUp()){
+                hibernateHandler.initSession(HibernateHandler.CreationMode.UPDATE);
+            } else {
+                hibernateHandler.initSession(HibernateHandler.CreationMode.CREATE);
+            }
+        } else {
+            Log.add(DEBUG_TAG, "No database connection available");
         }
     }
 
-    private void runHibernateWriteTest() {
-        hibernateHandler.writeTestLogic();
+    /**
+     * Checks if a database exists and creates one if not.
+     */
+    private boolean dataBaseSetUp(){
+        if(mySqlConnection == null){
+            mySqlConnection = new MySqlConnection();
+        }
+        if(!mySqlConnection.databaseExists()){
+            Log.add(DEBUG_TAG, "Database does not exist. Creating new.");
+            createDatabase();
+            return false;
+        }else {
+            return true;
+        }
     }
 
-    private void runHibernateReadTest(){
-        hibernateHandler.readLogic();
+    /**
+     * Checks if the server can connect to a database.
+     */
+    private boolean dataBaseAvailable(){
+        if(mySqlConnection == null){
+            mySqlConnection = new MySqlConnection();
+        }
+        return mySqlConnection.open();
     }
+
+    /**
+     * Create the database.
+     */
+    private void createDatabase(){
+        if(mySqlConnection.open()){
+            mySqlConnection.createDatabase();
+            mySqlConnection.close();
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //                                      LOGIC
+    //----------------------------------------------------------------------------------------------
+
+    public void writeLogic(Logic logic){
+
+    }
+
+    public List<Logic> readAllLogic(){
+        return null;
+    }
+
+    public void updateLogic(Logic logic){
+
+    }
+
+    public void deleteLogic(int id){
+
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //                                      TEST
+    //----------------------------------------------------------------------------------------------
+
+    public void runTest(){
+        writeTestLogic();
+        readLogic();
+    }
+
+
+    public void writeTestLogic(){
+
+        Actuator a = new Actuator();
+        a.setType(Actuator.TYPE_CLIENT);
+        a.setName("peters_nexus5");
+
+        Actuator b = new Actuator();
+        b.setType(Actuator.TYPE_MODULE);
+        b.setName("Heizung-Wohnzimmer");
+
+        Actuator c = new Actuator();
+        c.setType(Actuator.TYPE_SYSTEM);
+        c.setName("server_1.1.2");
+
+        Logic l1 = new Logic();
+        l1.setName("test_logic_666");
+        l1.setExecution_requirement(Logic.EXECUTION_REQUIREMENT_ALL);
+
+        ExecutionFrequency ef = new ExecutionFrequency();
+        ef.setType(ExecutionFrequency.TYPE_DAILY);
+        ef.setHour(17);
+        ef.setMinute(40);
+
+        l1.setExecution_frequency(ef);
+
+        LogicInitiator li = new LogicInitiator();
+        li.setActuator(a);
+        Condition co = new Condition();
+        co.setField_id(1);
+        co.setThreshold_over(24);
+        li.setCondition(co);
+
+        LogicInitiator li1 = new LogicInitiator();
+        li1.setActuator(b);
+        Condition co1 = new Condition();
+        co1.setField_id(2);
+        co1.setThreshold_under(10);
+        li1.setCondition(co1);
+
+        LogicReceiver lr = new LogicReceiver();
+        lr.setActuator(c);
+        Instruction i = new Instruction();
+        i.setField_id(12);
+        i.setParameters("hallo du wurst");
+        lr.setInstruction(i);
+
+        l1.getLogic_initiator().add(li);
+        l1.getLogic_initiator().add(li1);
+        l1.getLogic_receiver().add(lr);
+
+        hibernateHandler.insert(l1);
+    }
+
+    public void readLogic(){
+        for(Object o : hibernateHandler.readList(HibernateHandler.DataType.LOGIC)){
+            Logic l = (Logic) o;
+            printLogic(l);
+        }
+    }
+
+    public void printLogic(Logic l){
+        try {
+            String init = "[";
+            String rec = "[";
+
+            if (l.getLogic_initiator() != null && l.getLogic_initiator().size() > 0) {
+                for (LogicInitiator lii : l.getLogic_initiator()) {
+                    init += "(";
+                    init += " Actuator: " + lii.getActuator().getName();
+                    init += " Condition (fid): " + lii.getCondition().getField_id();
+                    init += ")";
+                }
+            }
+
+            init += "]";
+            if (l.getLogic_receiver() != null && l.getLogic_receiver().size() > 0) {
+                for (LogicReceiver lrr : l.getLogic_receiver()) {
+                    rec += "(";
+                    rec += " Actuator: " + lrr.getActuator().getName();
+                    rec += " Instruction (fid)" + lrr.getInstruction().getField_id();
+                    rec += ")";
+                }
+            }
+            rec += "]";
+
+            String freq = "[";
+            freq += " Type: " + l.getExecution_frequency().getType();
+            freq += " Hour: " + l.getExecution_frequency().getHour();
+            freq += " Minute: " + l.getExecution_frequency().getMinute();
+            freq += "]";
+
+            Log.add(DEBUG_TAG,
+                    "Found logic: " + l.getName()
+                            + " Id: " + l.getLogic_id()
+                            + " Frequency: " + freq
+                            + " Initiator: " + init
+                            + " Receiver: " + rec);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+
 
 }
