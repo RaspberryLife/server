2011a0f2d7919b6bda4ff0928e6a8e6394a00169
Peter Mösenthin <peter.moesenthin@gmail.com>
2015-02-04 19:45:15 +0100

    update user resolver

--- src/main/java/protobuf/ProtobufUserResolver.java
+++ src/main/java/protobuf/ProtobufUserResolver.java
@@ -1,7 +1,74 @@
 package protobuf;
 
+import client.RaspberryLifeClient;
+import data.model.User;
+
 /**
  * Created by Peter Mösenthin.
  */
 public class ProtobufUserResolver {
+
+    public static final String DEBUG_TAG = ProtobufUserResolver.class.getSimpleName();
+
+    private RaspberryLifeClient client;
+
+    public void resolve(RaspberryLifeClient client, RblProto.RBLMessage message){
+        this.client = client;
+        for(RblProto.RBLMessage.User user : message.getUserList()){
+            switch (user.getCrudType()){
+                case CREATE:
+                    createUser(user);
+                    break;
+                case RETRIEVE:
+                    retrieveUser(user);
+                    break;
+                case UPDATE:
+                    updateUser(user);
+                    break;
+                case DELETE:
+                    deleteUser(user);
+                    break;
+            }
+        }
+    }
+
+    private void createUser(RblProto.RBLMessage.User user){
+
+    }
+
+    private void retrieveUser(RblProto.RBLMessage.User user){
+
+    }
+
+    private void updateUser(RblProto.RBLMessage.User user){
+
+    }
+
+    private void deleteUser(RblProto.RBLMessage.User user){
+
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //                                      MAPPINGS
+    //----------------------------------------------------------------------------------------------
+
+    public static User mapModelUser(RblProto.RBLMessage.User user){
+        User u = new User();
+        if(user.hasId()){
+            u.setId(user.getId());
+        }
+        if(user.hasEmail()){
+            u.setEmail(user.getEmail());
+        }
+        if(user.hasPassword()){
+            u.setPassword(user.getPassword());
+        }
+        return u;
+    }
+
+    public static RblProto.RBLMessage.User mapProtobufUser(User user){
+        return ProtoFactory.buildUser(user.getId(),user.getEmail(), user.getPassword()).build();
+    }
+
+
 }
