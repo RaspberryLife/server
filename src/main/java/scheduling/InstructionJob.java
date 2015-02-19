823607c4f00f99de62681ea112e860ff9f044adc
Peter Mösenthin <peter.moesenthin@gmail.com>
2015-02-19 13:35:28 +0100

    implement job interface

--- src/main/java/scheduling/InstructionJob.java
+++ src/main/java/scheduling/InstructionJob.java
@@ -1,8 +1,19 @@
 package scheduling;
 
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
 /**
  * Created by Peter Mösenthin.
  */
-public class InstructionJob {
+public class InstructionJob  implements Job {
+
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+
+    }
+
 
 }
